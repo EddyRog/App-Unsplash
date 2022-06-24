791b96c1d0a6099e3c4fa9df36d03546d278d35c
Eddy.R <pro.eddy.r@gmail.com>
2022-06-24 16:56:29 +0200

    Setup project

---
+++ App-Unsplash/App-Unsplash/ViewController.swift
@@ -0,0 +1,19 @@
+//
+// ViewController.swift
+// App-Unsplash
+// Created in 2022
+// Swift 5.0
+
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
